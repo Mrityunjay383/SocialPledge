c6831a8a6d287107d71ae1681f76f96a1a6f4c0f
Mrityunjay383 <mrityunjayvyasmhl@gmail.com>
2023-07-25 03:57:04 +0530

    Supporter Dashboard Initial Done

--- client/src/Pages/Supporter/SupporterDashboard/index.js
+++ client/src/Pages/Supporter/SupporterDashboard/index.js
@@ -4,6 +4,21 @@ import { useNavigate, useParams } from "react-router-dom";
 import "./index.css";
 import { Supporter } from "../../../service";
 import { toast } from "react-toastify";
+import { ColorRing } from "react-loader-spinner";
+
+const Spinner = () => {
+  return (
+    <ColorRing
+      visible={true}
+      height="40"
+      width="40"
+      ariaLabel="blocks-loading"
+      wrapperStyle={{}}
+      wrapperClass="blocks-wrapper"
+      colors={["#FF5A60", "#FF5A60", "#FF5A60", "#FF5A60", "#FF5A60"]}
+    />
+  );
+};
 
 const SupporterDashboard = ({ supporterData, setIsLoggedIn }) => {
   const navigate = useNavigate();
@@ -19,6 +34,7 @@ const SupporterDashboard = ({ supporterData, setIsLoggedIn }) => {
   }, []);
 
   const [fetchedSupporterData, setFetchedSupporterData] = useState({});
+  const [isTopDataLoaded, setIsTopDataLoaded] = useState(false);
   const fetchSupporterData = async () => {
     const res = await Supporter.indieSup({
       userName: supporterData.userName,
@@ -27,6 +43,7 @@ const SupporterDashboard = ({ supporterData, setIsLoggedIn }) => {
     if (res.status === 200) {
       console.log(`#202320633619217 res.data.supporter`, res.data);
       setFetchedSupporterData(res.data.supporter);
+      setIsTopDataLoaded(true);
     } else {
       toast.error("Some Error occurred");
     }
@@ -47,9 +64,14 @@ const SupporterDashboard = ({ supporterData, setIsLoggedIn }) => {
               <h4 className="heading heading5 hind-font medium-font-weight c-dashboardInfo__title">
                 Total Limit
               </h4>
-              <span className="hind-font caption-12 c-dashboardInfo__count">
-                {fetchedSupporterData.newLimit}
-              </span>
+              {isTopDataLoaded ? (
+                <span className="hind-font caption-12 c-dashboardInfo__count">
+                  {fetchedSupporterData.newLimit}
+                </span>
+              ) : (
+                <Spinner />
+              )}
+
               <span className="hind-font caption-12 c-dashboardInfo__subInfo">
                 New Downloads
               </span>
@@ -60,9 +82,13 @@ const SupporterDashboard = ({ supporterData, setIsLoggedIn }) => {
               <h4 className="heading heading5 hind-font medium-font-weight c-dashboardInfo__title">
                 Total Limit
               </h4>
-              <span className="hind-font caption-12 c-dashboardInfo__count">
-                {fetchedSupporterData.repLimit}
-              </span>
+              {isTopDataLoaded ? (
+                <span className="hind-font caption-12 c-dashboardInfo__count">
+                  {fetchedSupporterData.repLimit}
+                </span>
+              ) : (
+                <Spinner />
+              )}
               <span className="hind-font caption-12 c-dashboardInfo__subInfo">
                 Repeat Downloads
               </span>
@@ -73,9 +99,13 @@ const SupporterDashboard = ({ supporterData, setIsLoggedIn }) => {
               <h4 className="heading heading5 hind-font medium-font-weight c-dashboardInfo__title">
                 New Users
               </h4>
-              <span className="hind-font caption-12 c-dashboardInfo__count">
-                {fetchedSupporterData.newCount}
-              </span>
+              {isTopDataLoaded ? (
+                <span className="hind-font caption-12 c-dashboardInfo__count">
+                  {fetchedSupporterData.newCount}
+                </span>
+              ) : (
+                <Spinner />
+              )}
               <span className="hind-font caption-12 c-dashboardInfo__subInfo">
                 Downloads
               </span>
@@ -86,9 +116,13 @@ const SupporterDashboard = ({ supporterData, setIsLoggedIn }) => {
               <h4 className="heading heading5 hind-font medium-font-weight c-dashboardInfo__title">
                 Repeat Users
               </h4>
-              <span className="hind-font caption-12 c-dashboardInfo__count">
-                {fetchedSupporterData.repCount}
-              </span>
+              {isTopDataLoaded ? (
+                <span className="hind-font caption-12 c-dashboardInfo__count">
+                  {fetchedSupporterData.repCount}
+                </span>
+              ) : (
+                <Spinner />
+              )}
               <span className="hind-font caption-12 c-dashboardInfo__subInfo">
                 Downloads
               </span>
